7a5986439312941f8894ff6e05fcca6eaa3c090d
wowoyinwei <82604458@qq.com>
2019-03-29 10:25:15 +0800

    nep5余额能录入mongo

---
+++ neo-debug/Plugins/Nep5State.cs
@@ -0,0 +1,11 @@
+﻿using System.Numerics;
+namespace Neo.Plugins
+{
+    public class Nep5State
+    {
+        public UInt160 Address;
+        public UInt160 AssetHash;
+        public BigInteger Balance;
+        public uint LastUpdatedBlock;
+    }
+}
